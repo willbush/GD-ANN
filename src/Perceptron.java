9be8193ccbe9eb45e92e00016c3f491aea0152ea
willbush <william.bush@utdallas.edu>
2016-10-01 17:44:44 -0500

    implement ability to apply the perceptron and print out stats and learning progression.

--- src/Perceptron.java
+++ src/Perceptron.java
@@ -2,9 +2,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Perceptron {
-    static void learn(DataSet dataSet, double learningRate, int numOfIterations) {
-        final int numOfAttributes = dataSet.getAttributeNames().size();
-        List<Double> weights = new ArrayList<>(dataSet.getAttributeNames().size());
+    static void learn(DataSet trainSet, DataSet testSet, double learningRate, int numOfIterations) {
+        final int numOfAttributes = trainSet.getAttributeNames().size();
+        List<Double> weights = new ArrayList<>(trainSet.getAttributeNames().size());
 
         for (int i = 0; i < numOfAttributes; ++i) {
             weights.add(0.0);
@@ -12,18 +12,54 @@ class Perceptron {
 
         int exampleIndex = 0;
         for (int i = 0; i < numOfIterations; ++i) {
-            if (exampleIndex >= dataSet.getExamples().size())
+            if (exampleIndex >= trainSet.getExamples().size())
                 exampleIndex = 0;
 
-            double dotProduct = dotProduct(dataSet.getExample(exampleIndex), weights);
-            double error = dataSet.getLabel(exampleIndex) - sigmoid(dotProduct);
+            double dotProduct = dotProduct(trainSet.getExample(exampleIndex), weights);
+            double error = trainSet.getLabel(exampleIndex) - sigmoid(dotProduct);
             double sigDerivative = sigmoidDerivative(dotProduct);
-            weights = updateWeights(weights, dataSet.getExample(exampleIndex), learningRate * sigDerivative * error);
-            double output = sigmoid((dotProduct(weights, dataSet.getExample(exampleIndex))));
+            weights = updateWeights(weights, trainSet.getExample(exampleIndex), learningRate * sigDerivative * error);
+            double output = sigmoid((dotProduct(weights, trainSet.getExample(exampleIndex))));
 
-            System.out.println(String.format("output: %.4f", output));
+            printLearningProgress(i + 1, trainSet.getAttributeNames(), weights, output);
             exampleIndex++;
         }
+
+        testAccuracy(trainSet, testSet, weights);
+    }
+
+    private static void testAccuracy(DataSet trainSet, DataSet testSet, List<Double> weights) {
+        final int trainInstanceCount = trainSet.getExamples().size();
+        final int testInstanceCount = testSet.getExamples().size();
+        int correctlyClassifiedTrainExamples = 0;
+        int correctlyClassifiedTestExamples = 0;
+
+        for (int i = 0; i < trainInstanceCount; ++i) {
+            double output = sigmoidDerivative(dotProduct(trainSet.getExample(i), weights));
+            if (Math.round(output) == Math.round(trainSet.getLabel(i))) {
+                correctlyClassifiedTrainExamples++;
+            }
+        }
+
+        for (int i = 0; i < testInstanceCount; ++i) {
+            double output = sigmoidDerivative(dotProduct(testSet.getExample(i), weights));
+            if (Math.round(output) == Math.round(testSet.getLabel(i))) {
+                correctlyClassifiedTestExamples++;
+            }
+        }
+        double trainAccuracy = (double) correctlyClassifiedTrainExamples / trainInstanceCount * 100;
+        double testAccuracy = (double) correctlyClassifiedTestExamples / testInstanceCount * 100;
+        System.out.println();
+        System.out.println(String.format("Accuracy on training set (%d instances): %.1f%%\n", trainInstanceCount, trainAccuracy));
+        System.out.println(String.format("Accuracy on test set (%d instances): %.1f%%", testInstanceCount, testAccuracy));
+    }
+
+    private static void printLearningProgress(int iterationNum, List<String> attributeNames, List<Double> weights, double output) {
+        System.out.print(String.format("After iteration %d: ", iterationNum));
+        for (int i = 0; i < weights.size(); ++i) {
+            System.out.print(String.format("w(%s) = %.4f, ", attributeNames.get(i), weights.get(i)));
+        }
+        System.out.println(String.format("output = %.4f", output));
     }
 
     private static List<Double> updateWeights(List<Double> weights, List<Double> attributes, double scalar) {
--- test/PerceptronTest.java
+++ test/PerceptronTest.java
@@ -1,11 +1,27 @@
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.util.*;
 
 import static org.junit.Assert.assertEquals;
 
 public class PerceptronTest {
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void setup() {
+        System.setOut(new PrintStream(out));
+    }
+
+    @After
+    public void tearDown() {
+        out.reset();
+    }
+
     @Test(expected = DataSet.EmptyFileException.class)
     public void canThrowIfGivenEmptyFile() throws IOException {
         final String emptyFileTest = "resources/dataFormatTest/emptyFile.dat";
@@ -35,12 +51,46 @@ public class PerceptronTest {
 
     @Test
     public void createPerceptron() {
-        final String dataSet = "A B C\n" +
+        final String dataSet = "Gee Fee Bee\n" +
                 "0 0 1 0\n" +
                 "1 0 1 0\n" +
                 "0 1 1 0\n" +
                 "1 1 1 1\n";
-        Perceptron.learn(convertToSet(dataSet), 0.3, 30);
+        final String expectedOutput = "After iteration 1: w(Gee) = 0.0000, w(Fee) = 0.0000, w(Bee) = -0.0375, output = 0.4906\n" +
+                "After iteration 2: w(Gee) = -0.0368, w(Fee) = 0.0000, w(Bee) = -0.0743, output = 0.4723\n" +
+                "After iteration 3: w(Gee) = -0.0368, w(Fee) = -0.0361, w(Bee) = -0.1103, output = 0.4635\n" +
+                "After iteration 4: w(Gee) = 0.0038, w(Fee) = 0.0045, w(Bee) = -0.0698, output = 0.4846\n" +
+                "After iteration 5: w(Gee) = 0.0038, w(Fee) = 0.0045, w(Bee) = -0.1059, output = 0.4735\n" +
+                "After iteration 6: w(Gee) = -0.0317, w(Fee) = 0.0045, w(Bee) = -0.1414, output = 0.4568\n" +
+                "After iteration 7: w(Gee) = -0.0317, w(Fee) = -0.0302, w(Bee) = -0.1762, output = 0.4486\n" +
+                "After iteration 8: w(Gee) = 0.0097, w(Fee) = 0.0111, w(Bee) = -0.1348, output = 0.4715\n" +
+                "After iteration 9: w(Gee) = 0.0097, w(Fee) = 0.0111, w(Bee) = -0.1696, output = 0.4577\n" +
+                "After iteration 10: w(Gee) = -0.0246, w(Fee) = 0.0111, w(Bee) = -0.2039, output = 0.4431\n" +
+                "After iteration 11: w(Gee) = -0.0246, w(Fee) = -0.0225, w(Bee) = -0.2375, output = 0.4354\n" +
+                "After iteration 12: w(Gee) = 0.0173, w(Fee) = 0.0195, w(Bee) = -0.1956, output = 0.4604\n" +
+                "After iteration 13: w(Gee) = 0.0173, w(Fee) = 0.0195, w(Bee) = -0.2291, output = 0.4430\n" +
+                "After iteration 14: w(Gee) = -0.0159, w(Fee) = 0.0195, w(Bee) = -0.2623, output = 0.4309\n" +
+                "After iteration 15: w(Gee) = -0.0159, w(Fee) = -0.0130, w(Bee) = -0.2947, output = 0.4237\n" +
+                "After iteration 16: w(Gee) = 0.0265, w(Fee) = 0.0294, w(Bee) = -0.2523, output = 0.4510\n" +
+                "After iteration 17: w(Gee) = 0.0265, w(Fee) = 0.0294, w(Bee) = -0.2846, output = 0.4293\n" +
+                "After iteration 18: w(Gee) = -0.0056, w(Fee) = 0.0294, w(Bee) = -0.3168, output = 0.4201\n" +
+                "After iteration 19: w(Gee) = -0.0056, w(Fee) = -0.0021, w(Bee) = -0.3483, output = 0.4133\n" +
+                "After iteration 20: w(Gee) = 0.0371, w(Fee) = 0.0406, w(Bee) = -0.3055, output = 0.4433\n" +
+                "After iteration 21: w(Gee) = 0.0371, w(Fee) = 0.0406, w(Bee) = -0.3366, output = 0.4166\n" +
+                "After iteration 22: w(Gee) = 0.0059, w(Fee) = 0.0406, w(Bee) = -0.3678, output = 0.4105\n" +
+                "After iteration 23: w(Gee) = 0.0059, w(Fee) = 0.0100, w(Bee) = -0.3984, output = 0.4041\n" +
+                "After iteration 24: w(Gee) = 0.0489, w(Fee) = 0.0530, w(Bee) = -0.3554, output = 0.4370\n" +
+                "After iteration 25: w(Gee) = 0.0489, w(Fee) = 0.0530, w(Bee) = -0.3854, output = 0.4048\n" +
+                "After iteration 26: w(Gee) = 0.0185, w(Fee) = 0.0530, w(Bee) = -0.4158, output = 0.4020\n" +
+                "After iteration 27: w(Gee) = 0.0185, w(Fee) = 0.0232, w(Bee) = -0.4455, output = 0.3960\n" +
+                "After iteration 28: w(Gee) = 0.0617, w(Fee) = 0.0664, w(Bee) = -0.4024, output = 0.4319\n" +
+                "After iteration 29: w(Gee) = 0.0617, w(Fee) = 0.0664, w(Bee) = -0.4312, output = 0.3938\n" +
+                "After iteration 30: w(Gee) = 0.0321, w(Fee) = 0.0664, w(Bee) = -0.4609, output = 0.3944\n\n" +
+                "Accuracy on training set (4 instances): 75.0%\n\n" +
+                "Accuracy on test set (4 instances): 75.0%\n";
+        DataSet trainingAndTest = convertToSet(dataSet); // training and test same are the same in this case.
+        Perceptron.learn(trainingAndTest, trainingAndTest, 0.3, 30);
+        assertEquals(expectedOutput, out.toString());
     }
 
     private static DataSet convertToSet(String data) {
